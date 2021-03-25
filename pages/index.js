9bbce8401b3819cb312b23e403c7e3ca307ef454
xsamskellington <samantha.valdez.toccalino@gmail.com>
2021-03-24 18:39:32 -0600

    fixing the filter

--- pages/index.js
+++ pages/index.js
@@ -22,16 +22,16 @@ export default function Home({ pokemon }) {
         />
       </div>
       <ul>
-        {pokeName.map((pokeman, index) => (
-          <li key={index}>
-            <Link href={`/pokemon?id=${index + 1}`}>
+        {pokeName.map((pokeman) => (
+          <li key={pokeman.name}>
+            <Link href={`/pokemon?id=${pokeman.pokeIndex}`}>
               <a className="border p-4 my-2 capitalize flex items-center text-lg bg-gray-200 rounded-md">
                 <img
                   className="w-20 h-20 mr-3"
                   src={pokeman.image}
                   alt={pokeman.name}
                 />
-                <span className="mr-2 font-bold">{index + 1}.</span>
+                <span className="mr-2 font-bold">{pokeman.pokeIndex}.</span>
                 {pokeman.name}
               </a>
             </Link>
@@ -48,11 +48,13 @@ export async function getStaticProps(context) {
     const { results } = await res.json();
     const pokemon = results.map((pokeman, index) => {
       const paddedIndex = ('00' + (index + 1)).slice(-3);
+      const pokeIndex = index + 1;
       const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedIndex}.png`;
 
       return {
         ...pokeman,
         image,
+        pokeIndex,
       };
     });
 
